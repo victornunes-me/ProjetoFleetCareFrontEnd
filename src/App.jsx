79dad3c0a5fb2d4ffa554c01c69aad5baad1df6f
Victor Nunes <victor.engbiotec@gmail.com>
2023-08-06 14:04:58 -0300

    autenticação de user + ajustes de rotas + validação de login

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 //componentes
-import Navbar from './components/navbar'
+import AuthNavbar from './components/authNavbar'
 import { Box } from '@mui/material'
 import { ThemeProvider} from '@mui/material/styles';
 import { theme } from './theme'
@@ -17,50 +17,129 @@ import Usuarios from './pages/usuarios'
 import Inicio from './pages/inicio'
 import NotFound from './notFound'
 import Usuario from './pages/usuario'
+import { AuthProvider } from './context/AuthProvider'
+import { ProtectedLayout } from './components/ProtectedLayout'
 
 
 function App() {
-  const [isActiveLogin, setIsActiveLogin] = useState(0)
-
-  if (isActiveLogin == 1){
     return (
       <>
+      <AuthProvider>
       <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="*" element={<NotFound/>} />
-        <Route path='/ProjetoFleetCareFrontEnd/index.html:inicio' element = {<Login/>}/>
-      </Routes>
-      </ThemeProvider>
-      </>
-    )
-  }else{
-    return (
-      <>
-      <ThemeProvider theme={theme}>
-      
+        
       <Box>
-        <Box className='flex' sx={{ position:'sticky' }}><Navbar /></Box>
-        <Box className='boxContent'>
             <Routes basename='/ProjetoFleetCareFrontEnd'>
-              <Route path="/ProjetoFleetCareFrontEnd/*" element={<NotFound/>} />
-              <Route exact path='/ProjetoFleetCareFrontEnd/' element = {<Inicio/>}/>
-              <Route exact path='/ProjetoFleetCareFrontEnd/login' element = {<Login/>}/>
+            
+            //Login e 404 
+            <Route exact path='/ProjetoFleetCareFrontEnd/login' element = {<Login/>}/>
+            <Route path="/ProjetoFleetCareFrontEnd/*" element={<NotFound/>}/>
+             
+            //inicio 
+            <Route exact path='/ProjetoFleetCareFrontEnd/' 
+              element = {
+              <ProtectedLayout>
+                 <Box className='flex' sx={{ position:'sticky' }}><AuthNavbar/></Box>
+                  <Box className='boxContent'> <Inicio/></Box>
+              </ProtectedLayout>
+              }/>
 
-              <Route exact path='/ProjetoFleetCareFrontEnd/usuarios' element = {<Usuarios/>}/>
-              <Route path='/ProjetoFleetCareFrontEnd/usuario/*' element = {<Usuario/>}/>
+            //Usuarios 
+              <Route exact path='/ProjetoFleetCareFrontEnd/usuarios' 
+              element = {
+              <ProtectedLayout>
+                <Box className='flex' sx={{ position:'sticky' }}><AuthNavbar/></Box>
+                <Box className='boxContent'>  <Usuarios/> </Box>
+              </ProtectedLayout>
+              }/>
+
+            //Usuario 
+              <Route exact path='/ProjetoFleetCareFrontEnd/usuario/*' 
+              element = {
+              <ProtectedLayout>
+                <Box className='flex' sx={{ position:'sticky' }}><AuthNavbar/></Box>
+                <Box className='boxContent'>  <Usuario/> </Box>
+              </ProtectedLayout>
+              }/>
               
             </Routes>
-        </Box>
-        <Box></Box>
       </Box>
-      
       </ThemeProvider>
-    
+      </AuthProvider>
       </>
     )
 
   }
-
-}
-
 export default App
+
+
+
+
+
+// //react
+// import { useState } from 'react'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// //arquivos
+// import './App.css'
+
+// //componentes
+// import Navbar from './components/navbar'
+// import { Box } from '@mui/material'
+// import { ThemeProvider} from '@mui/material/styles';
+// import { theme } from './theme'
+
+// //pages
+// import Login from './pages/login'
+// import Usuarios from './pages/usuarios'
+// import Inicio from './pages/inicio'
+// import NotFound from './notFound'
+// import Usuario from './pages/usuario'
+// import { AuthProvider } from './context/AuthProvider'
+
+
+// function App() {
+//   const [isActiveLogin, setIsActiveLogin] = useState(0)
+
+//   if (isActiveLogin == 0){
+//     return (
+//       <>
+//       <ThemeProvider theme={theme}>
+//       <Routes>
+//         <Route path="*" element={<NotFound/>} />
+//         <Route path='/ProjetoFleetCareFrontEnd/login' element = {<Login/>}/>
+//       </Routes>
+//       </ThemeProvider>
+//       </>
+//     )
+//   }else{
+//     return (
+//       <>
+//       <AuthProvider>
+//       <ThemeProvider theme={theme}>
+      
+//       <Box>
+//         <Box className='flex' sx={{ position:'sticky' }}><Navbar /></Box>
+//         <Box className='boxContent'>
+//             <Routes basename='/ProjetoFleetCareFrontEnd'>
+//               <Route path="/ProjetoFleetCareFrontEnd/*" element={<NotFound/>} />
+//               <Route exact path='/ProjetoFleetCareFrontEnd/' element = {<Inicio/>}/>
+//               <Route exact path='/ProjetoFleetCareFrontEnd/login' element = {<Login/>}/>
+
+//               <Route exact path='/ProjetoFleetCareFrontEnd/usuarios' element = {<Usuarios/>}/>
+//               <Route path='/ProjetoFleetCareFrontEnd/usuario/*' element = {<Usuario/>}/>
+              
+//             </Routes>
+//         </Box>
+//         <Box></Box>
+//       </Box>
+      
+//       </ThemeProvider>
+//       </AuthProvider>
+//       </>
+//     )
+
+//   }
+
+// }
+
+// export default App
--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/ProjetoFleetCareFrontEnd/",
-
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
-      },
-    ];
+  server: {
+    cors: false,
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: '/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         { key: 'Access-Control-Allow-Origin', value: '*' },
+  //         { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+  //         { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+  //       ],
+  //     },
+  //   ];
+  // },
 
 })
